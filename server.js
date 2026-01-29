0182622648f52bdfb8ea317636c6503f691438f2
Abdelrahman744 <ww.ab472005@gmail.com>
2026-01-29 04:56:23 +0200

    .

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import { app } from "./app.js";
+import dotenv from "dotenv";
+const mongoose = require("mongoose");
 
-const port = 3000;
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port} ......... `);
 });
