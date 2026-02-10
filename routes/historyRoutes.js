733b625e5b9b14e0f6c147624e16cc0083d11556
Abdelrahman744 <ww.ab472005@gmail.com>
2026-02-10 09:54:53 +0200

    enhanced comments

--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -3,13 +3,18 @@ import {
   getUserHistory,
   getBookHistory,
 } from "../controllers/historyController.js";
-// Import 'protect' here
+
 import { protect, restrictTo } from "./../middleware/auth.js";
 
 const Router = express.Router();
 
+
+// authenticated routes 
+
 Router.get("/users/:id", protect, getUserHistory);
 
-Router.get("/books/:id", protect, restrictTo("admin"), getBookHistory);
+// admin only routes 
+
+Router.get("/books/:id", protect, restrictTo, getBookHistory);
 
 export default Router;
