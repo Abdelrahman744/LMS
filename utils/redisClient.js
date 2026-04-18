300b6017ccf71d58a5a01d5580b3e9aae712e04d
Abdelrahman744 <ww.ab472005@gmail.com>
2026-04-18 16:06:49 +0200

    fix: load dotenv inside redis client to resolve localhost routing error

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,11 +1,13 @@
 import { createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const redisClient = createClient({
     username: 'default',
-    password: '4NKGRf5mOVui4V14snt8958AZSpXqBgd',
+    password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-13412.c11.us-east-1-3.ec2.cloud.redislabs.com',
-        port: 13412
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
     }
 });
 
@@ -13,6 +15,5 @@ redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
 await redisClient.connect();
 console.log("Connected to Redis");
-
 
 export default redisClient;
